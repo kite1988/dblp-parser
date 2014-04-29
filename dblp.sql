b1c9ae35e785ed13aef2f3769d727546dc184fda
Tao Chen <taochen@comp.nus.edu.sg>
2014-04-29 11:04:36 +0800

    update sqldump file by changing paper_key to varchar(200)

--- dblp.sql
+++ dblp.sql
@@ -1,18 +1,19 @@
--- MySQL Administrator dump 1.4
+-- MySQL dump 10.13  Distrib 5.6.11, for Win64 (x86_64)
 --
+-- Host: localhost    Database: dblp
 -- ------------------------------------------------------
 -- Server version	5.6.11
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Create schema dblp
@@ -22,81 +23,60 @@ CREATE DATABASE IF NOT EXISTS dblp;
 USE dblp;
 
 --
--- Definition of table `author`
+-- Table structure for table `author`
 --
 
 DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `author` (
   `name` varchar(100) DEFAULT NULL,
-  `paper_key` varchar(100) DEFAULT NULL
+  `paper_key` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `author`
---
-
-/*!40000 ALTER TABLE `author` DISABLE KEYS */;
-/*!40000 ALTER TABLE `author` ENABLE KEYS */;
-
-
---
--- Definition of table `citation`
+-- Table structure for table `citation`
 --
 
 DROP TABLE IF EXISTS `citation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `citation` (
-  `paper_cite_key` varchar(100) DEFAULT NULL,
-  `paper_cited_key` varchar(100) DEFAULT NULL
+  `paper_cite_key` varchar(200) DEFAULT NULL,
+  `paper_cited_key` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `citation`
---
-
-/*!40000 ALTER TABLE `citation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `citation` ENABLE KEYS */;
-
-
---
--- Definition of table `conference`
+-- Table structure for table `conference`
 --
 
 DROP TABLE IF EXISTS `conference`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `conference` (
   `conf_key` varchar(100) DEFAULT NULL,
   `name` text,
   `detail` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `conference`
---
-
-/*!40000 ALTER TABLE `conference` DISABLE KEYS */;
-/*!40000 ALTER TABLE `conference` ENABLE KEYS */;
-
-
---
--- Definition of table `paper`
+-- Table structure for table `paper`
 --
 
 DROP TABLE IF EXISTS `paper`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `paper` (
   `title` text,
   `year` int(11) DEFAULT '0',
   `conference` text,
-  `paper_key` varchar(120) DEFAULT NULL
+  `paper_key` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `paper`
---
-
-/*!40000 ALTER TABLE `paper` DISABLE KEYS */;
-/*!40000 ALTER TABLE `paper` ENABLE KEYS */;
-
-
-
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -104,4 +84,6 @@ CREATE TABLE `paper` (
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-04-29 11:02:04
